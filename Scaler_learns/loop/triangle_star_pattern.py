c0029acc2ef9049065528a0fba78f2374a01eacc
Ristolive <ristolive@gmail.com>
2023-12-06 07:50:28 +0000

    push

--- Scaler_learns/loop/triangle_star_pattern.py
+++ Scaler_learns/loop/triangle_star_pattern.py
@@ -1,5 +1,8 @@
 n=int(input("Enter Row: "))
+
 for i in range(1,n+1):
-    for x in range(i):
+    for x in range(n-i):
+        print(" ", end=" ")
+    for x in range(2*i-1):
         print("*", end=" ")
-    print()
+    print("\n", end="")
